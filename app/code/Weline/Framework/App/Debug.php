b30672ea0bd35da5e4a3231d73f16132acf73092
秋枫雁飞 <1714255949@qq.com>
2025-01-08 10:14:48 +0800

    调整识别debug

--- app/code/Weline/Framework/App/Debug.php
+++ app/code/Weline/Framework/App/Debug.php
@@ -10,11 +10,17 @@ class Debug
         return $value;
     }
 
-    public static function target(string $env_key, mixed $value = ''): mixed
+    public static function target(string $env_key, mixed $value = null): mixed
     {
         # 无值看看是否有键名
         if (!$value) {
-            return isset($_ENV['w-debug'][$env_key]);
+            if (!isset($_ENV['w-debug'])) {
+                return false;
+            }
+            if(array_key_exists($env_key, $_ENV['w-debug'])){
+                return true;
+            }
+            return false;
         }
         # 有值看看值是否相等
         $env_value = $_ENV['w-debug'][$env_key] ?? null;
