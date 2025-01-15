eda1cbfab1c1084897950a67f6e9ecaf031b48c7
Aiweline <32635766+Aiweline@users.noreply.github.com>
2025-01-15 16:52:29 +0800

    添加产品商铺模块

---
+++ app/code/Gvanda/Product/Cache/BackendCacheFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Gvanda\Product\Cache;
+
+use Weline\Framework\Cache\CacheFactory;
+
+class BackendCacheFactory extends CacheFactory
+{
+    public function __construct($identity = 'product_backend_cache', $tip = '产品后台缓存', $permanently = false)
+    {
+        parent::__construct($identity, $tip, $permanently);
+    }
+}
