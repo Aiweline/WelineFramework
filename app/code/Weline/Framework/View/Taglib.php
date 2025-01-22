c4df77cbc759d0f6940b53f9e7212c41205edb02
秋枫雁飞 <1714255949@qq.com>
2025-01-22 17:16:35 +0800

    遗漏补充

--- app/code/Weline/Framework/View/Taglib.php
+++ app/code/Weline/Framework/View/Taglib.php
@@ -616,8 +616,15 @@ class Taglib
             ],
             'template' => [
                 'tag' => 1,
+                'attr' => ['enable' => 0],
                 'callback' =>
                     function ($tag_key, $config, $tag_data, $attributes) use ($template) {
+                        $enable = $attributes['enable'] ?? 1;
+                        if (!$enable or ($enable ==='false')) {
+                            $template_string  = $tag_data[0]??'';
+                            $target_template  = $tag_data[2]??'';
+                            return "<!-- 模块被禁用：{$target_template} 原始模板：{$template_string}-->";
+                        }
                         return match ($tag_key) {
                             'tag' => file_get_contents($template->fetchTagSource(\Weline\Framework\View\Data\DataInterface::dir_type_TEMPLATE, trim($tag_data[2]))),
                             default => file_get_contents($template->fetchTagSource(\Weline\Framework\View\Data\DataInterface::dir_type_TEMPLATE, trim($tag_data[1])))
