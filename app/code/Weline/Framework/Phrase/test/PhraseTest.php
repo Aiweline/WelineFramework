42f85396546caa0c80aa3fae332fd902474506a0
Aiweline <32635766+Aiweline@users.noreply.github.com>
2025-06-02 12:38:53 +0800

    '修改翻译占位符'

--- app/code/Weline/Framework/Phrase/test/PhraseTest.php
+++ app/code/Weline/Framework/Phrase/test/PhraseTest.php
@@ -2,28 +2,29 @@
 
 namespace Weline\Framework\Phrase\test;
 
+use Weline\Framework\App\Debug;
 use Weline\Framework\UnitTest\TestCore;
 
 class PhraseTest extends TestCore
 {
     public function testPhrase()
     {
-        $word = "Hello, world%1!";
+        $word = "Hello, world%{1}!";
         $res_args = array("Weline");
         $actual = \Weline\Framework\Phrase\Parser::parse($word, $res_args);
         $expected = "Hello, worldWeline!";
         $this->assertEquals($expected, $actual);
 
-        $word = "Hello, world%1!";
+        $word = "Hello, world%{}!";
         $res_args = '1fdf';
         $actual = \Weline\Framework\Phrase\Parser::parse($word, $res_args);
         $expected = "Hello, world1fdf!";
         $this->assertEquals($expected, $actual);
 
-        $word = "Hello, world%1!%2,%3";
-        $res_args = array("Weline", "Hello, world%1!", "Weline");
+        $word = "Hello, world%{1}!%{2},%{3}";
+        $res_args = array("Weline", "Hello, world%{1}!", "Weline");
         $actual = \Weline\Framework\Phrase\Parser::parse($word, $res_args);
-        $expected = "Hello, worldWeline!Hello, world%1!,Weline";
+        $expected = "Hello, worldWeline!Hello, world%{1}!,Weline";
         $this->assertEquals($expected, $actual);
 
     }
