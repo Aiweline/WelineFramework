f92c20899d7cb940d097eec6fa7bc83f783e3ca8
秋枫雁飞 <1714255949@qq.com>
2025-06-12 23:35:19 +0800

    监听文件管理器程序自动生成的js配置文件

--- app/code/Weline/ElFinderFileManager/view/statics/elfinder-backend-dev-main.js
+++ app/code/Weline/ElFinderFileManager/view/statics/elfinder-backend-dev-main.js
@@ -46,7 +46,7 @@
 		// Start elFinder (REQUIRED)
 		start = function(elFinder, editors, config) {
 			// load jQueryUI CSS
-			elFinder.prototype.loadCss('//cdnjs.cloudflare.com/ajax/libs/jqueryui/'+uiver+'/themes/smoothness/jquery-ui.css');
+			elFinder.prototype.loadCss('/Weline/ElFinderFileManager/view/statics/jquery-ui.min.css');
 			
 			$(function() {
 				var optEditors = {
@@ -124,8 +124,8 @@
 	require.config({
 		baseUrl : '/Weline/ElFinderFileManager/view/statics/js',
 		paths : {
-			'jquery'   : '//cdnjs.cloudflare.com/ajax/libs/jquery/'+(old? '1.12.4' : jqver)+'/jquery.min',
-			'jquery-ui': '//cdnjs.cloudflare.com/ajax/libs/jqueryui/'+uiver+'/jquery-ui.min',
+			'jquery'   : '/Weline/ElFinderFileManager/view/statics/jquery.min',
+			'jquery-ui': '/Weline/ElFinderFileManager/view/statics/jquery-ui.min',
 			'elfinder' : 'elfinder.min',
 			'encoding-japanese': '//cdn.rawgit.com/polygonplanet/encoding.js/1.0.26/encoding.min'
 		},
