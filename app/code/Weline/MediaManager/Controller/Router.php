a6f15a62c41d19f14fc6b91880e27abbf9811181
秋枫雁飞 <1714255949@qq.com>
2025-06-14 01:33:26 +0800

    处理文件url

--- app/code/Weline/MediaManager/Controller/Router.php
+++ app/code/Weline/MediaManager/Controller/Router.php
@@ -14,29 +14,17 @@ class Router implements RouterInterface
      */
     public static function process(string &$path, array &$rule): void
     {
-        # 匹配静态资源/static/
-        if (str_starts_with(strtolower($path), '/static/')) {
-            $path = '/pub' . $path;
-            if (is_file(BP . $path)) {
-                /**@var Core $core */
-                $core = ObjectManager::getInstance(Core::class);
-                $core->StaticFile($path, true);
-                exit;
+        if($_SERVER['WELINE_IS_MEDIA']){
+            if (str_starts_with(strtolower($path), '/media/image/')) {
+                $file = str_replace('/media/image/', '', $path);
+                $rule['file'] = urldecode($file);
+                $path = '/media/image/index';
             }
-        }
-        # 匹配媒介资源
-        if (str_starts_with(strtolower($path), '/pub/media/')) {
-            if (is_file(BP . $path)) {
-                /**@var Core $core */
-                $core = ObjectManager::getInstance(Core::class);
-                $core->StaticFile($path, true);
-                exit;
+            if (str_starts_with(strtolower($path), '/media/file/')) {
+                $file = str_replace('/media/file/', '', $path);
+                $rule['file'] = urldecode($file);
+                $path = '/media/file/index';
             }
-        }
-        if (str_starts_with(strtolower($path), '/media/image/')) {
-            $file = str_replace('/media/image/', '', $path);
-            $rule['file'] = $file;
-            $path = '/media/image/index';
         }
     }
 }
