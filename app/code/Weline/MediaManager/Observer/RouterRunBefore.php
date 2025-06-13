6ea79edd2629175cb0282b7b10d256ac91d076be
秋枫雁飞 <aiweline@qq.com>
2025-06-14 01:33:39 +0800

    处理文件url

---
+++ app/code/Weline/MediaManager/Observer/RouterRunBefore.php
@@ -0,0 +1,60 @@
+<?php
+
+
+namespace Weline\MediaManager\Observer;
+
+use Weline\Framework\Event\ObserverInterface;
+use Weline\Framework\Event\Event;
+use Weline\Framework\Manager\ObjectManager;
+use Weline\Framework\Router\Core;
+
+class RouterRunBefore implements ObserverInterface
+{
+    public function execute(Event &$event)
+    {
+        $path = strtolower($_SERVER['REQUEST_URI']??'');
+        # 匹配静态资源/static/
+        if (str_starts_with($path, '/static/')) {
+            $file_path = BP .'/pub' . $path;
+            if(IS_WIN){
+                $file_path = str_replace('/','\\',$file_path);
+                $file_path = str_replace('\\\\','\\',$file_path);
+            }else{
+                $file_path = str_replace('//','/',$file_path);
+            }
+            if (is_file($file_path)) {
+                /**@var Core $core */
+                $core = ObjectManager::getInstance(Core::class);
+                $core->StaticFile( $path, true);
+                exit;
+            }
+        }
+        # 匹配媒介资源
+        if (str_starts_with($path, '/pub/media/')) {
+            $file_path = BP.urldecode($path);
+            if(IS_WIN){
+                $file_path = str_replace('/','\\',$file_path);
+                $file_path = str_replace('\\\\','\\',$file_path);
+            }else{
+                $file_path = str_replace('//','/',$file_path);
+            }
+            if (is_file($file_path)) {
+                /**@var Core $core */
+                $core = ObjectManager::getInstance(Core::class);
+                $core->StaticFile( $path, true);
+                exit;
+            }
+        }
+        // 跳过解析 
+        // 图片
+        if (str_starts_with($path, '/media/image/')) {
+            $_SERVER['WELINE_PARSER_URL'] = false;
+            $_SERVER['WELINE_IS_MEDIA'] = true;
+        }
+        // 文件
+        if (str_starts_with($path, '/media/file/')) {
+            $_SERVER['WELINE_PARSER_URL'] = false;
+            $_SERVER['WELINE_IS_MEDIA'] = true;
+        }
+    }
+}
