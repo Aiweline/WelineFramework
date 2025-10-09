045e63d32e58d072eb253cb3113495e45a94c82a
秋枫雁飞 <aiweline@qq.com>
2025-10-10 03:03:26 +0800

    fix(warmcache): 更新 WarmCache 缓存预热命令
    改进缓存预热命令的并发与错误处理。

--- app/code/Weline/WarmCache/Console/Cache/Warm.php
+++ app/code/Weline/WarmCache/Console/Cache/Warm.php
@@ -62,4 +62,18 @@ class Warm implements CommandInterface
     {
         return __('提供缓存预热,加速网页访问');
     }
+
+    public function help(): array|string
+    {
+        // 基于tip的默认help实现
+        return \Weline\Framework\Console\CommandHelper::formatHelp(
+            '',
+            $this->tip(),
+            [
+                '-h, --help' => '显示帮助信息',
+            ],
+            [],
+            []
+        );
+    }
 }
