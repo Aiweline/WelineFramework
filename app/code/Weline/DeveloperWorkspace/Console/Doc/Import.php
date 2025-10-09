702a0829c6a55c512b923fbd6b8f68070d9eb051
秋枫雁飞 <aiweline@qq.com>
2025-10-10 03:03:25 +0800

    chore(devworkspace): 更新 DeveloperWorkspace 文档导入命令
    改进开发者工作空间文档导入命令，修复路径解析与错误提示。

--- app/code/Weline/DeveloperWorkspace/Console/Doc/Import.php
+++ app/code/Weline/DeveloperWorkspace/Console/Doc/Import.php
@@ -25,6 +25,20 @@ class Import extends CommandAbstract
     {
         return '触发模块文档导入任务';
     }
+
+    public function help(): array|string
+    {
+        // 基于tip的默认help实现
+        return \Weline\Framework\Console\CommandHelper::formatHelp(
+            '',
+            $this->tip(),
+            [
+                '-h, --help' => '显示帮助信息',
+            ],
+            [],
+            []
+        );
+    }
 }
 
 
