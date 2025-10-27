c0b9f0a73d8df79beac16a2d169019a82bf78d76
秋枫雁飞 <1714255949@qq.com>
2025-10-27 20:46:10 +0800

    fix: add void return type to remaining Observer execute methods

--- app/code/Weline/Frontend/Observer/Footer.php
+++ app/code/Weline/Frontend/Observer/Footer.php
@@ -18,7 +18,7 @@ class Footer implements \Weline\Framework\Event\ObserverInterface
     /**
      * @inheritDoc
      */
-    public function execute(Event &$event)
+    public function execute(Event &$event): void
     {
         if (!$event->getData('is_backend')) {
             $event->setData('class', \Weline\Frontend\Model\Html\Footer::class);
