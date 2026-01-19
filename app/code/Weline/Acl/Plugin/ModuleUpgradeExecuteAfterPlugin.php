197021794256ec028864e34b936fc59f8b08d807
绉嬫灚闆侀 <1714255949@qq.com>
2026-01-19 17:46:42 +0800

    Weline_Acl: 修改 10 个文件

--- app/code/Weline/Acl/Plugin/ModuleUpgradeExecuteAfterPlugin.php
+++ app/code/Weline/Acl/Plugin/ModuleUpgradeExecuteAfterPlugin.php
@@ -25,12 +25,24 @@ class ModuleUpgradeExecuteAfterPlugin
     {
         $this->acl = $acl;
     }
+    /**
+     * 在模块升级执行前清空ACL表
+     * 
+     * 注意：此方法在系统升级开始前执行，清空ACL表以便重新收集所有权限
+     * 权限收集是在升级过程中通过 ControllerAttributes 观察者进行的
+     */
     function beforeExecute()
     {
-        /**@var Setup $setup*/
-        $setup = ObjectManager::getInstance(Setup::class);
-        if($setup->setConnection($this->acl->getConnection())->tableExist($this->acl->getTable())){
-            $this->acl->query("TRUNCATE TABLE {$this->acl->getTable()}");
+        try {
+            // 清空ACL表，以便重新收集所有权限
+            if ($this->acl->getTable()) {
+                $this->acl->query("TRUNCATE TABLE {$this->acl->getTable()}");
+            }
+        } catch (\Exception $e) {
+            // 清空表失败不影响升级流程，只记录错误
+            if (defined('DEV') && DEV) {
+                error_log("清空ACL表失败: " . $e->getMessage());
+            }
         }
     }
 }
