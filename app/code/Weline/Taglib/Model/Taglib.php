09fb03f2ed0ce3252491c391f6fb82139febc5f6
秋枫雁飞 <1714255949@qq.com>
2026-03-05 11:18:45 +0800

    ```

--- app/code/Weline/Taglib/Model/Taglib.php
+++ app/code/Weline/Taglib/Model/Taglib.php
@@ -1,21 +1,34 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Weline\Taglib\Model;
 
-use Weline\Framework\Database\Api\Db\TableInterface;
 use Weline\Framework\Database\Model;
-use Weline\Framework\Setup\Data\Context;
-use Weline\Framework\Setup\Db\ModelSetup;
+use Weline\Framework\Database\Schema\Attribute\Col;
+use Weline\Framework\Database\Schema\Attribute\Index;
+use Weline\Framework\Database\Schema\Attribute\Table;
 use Weline\ModuleManager\Model\Module;
 
+#[Table(comment: '标签表')]
+#[Index(name: 'idx_name', columns: ['name'], type: 'UNIQUE')]
 class Taglib extends Model
 {
-    public const fields_ID = 'taglib_id';
-    public const fields_MODULE_ID = 'module_id';
-    public const fields_NAME = 'name';
-    public const fields_DESCRIPTION = 'description';
-    public const fields_JSON = 'json';
-    public const fields_is_system = 'is_system';
+
+    public const schema_table = 'taglib';
+    public const schema_primary_key = 'taglib_id';
+    #[Col('int', 11, primaryKey: true, autoIncrement: true, nullable: false, comment: '标签ID')]
+    public const schema_fields_ID = 'taglib_id';
+    #[Col('int', 11, nullable: false, comment: '标签模型ID')]
+    public const schema_fields_MODULE_ID = 'module_id';
+    #[Col('varchar', 60, nullable: false, unique: true, comment: '标签')]
+    public const schema_fields_NAME = 'name';
+    #[Col('text', comment: 'Taglib 详情')]
+    public const schema_fields_DESCRIPTION = 'description';
+    #[Col('text', comment: 'Taglib数据')]
+    public const schema_fields_JSON = 'json';
+    #[Col('int', 1, default: 0, comment: '是否系统标签')]
+    public const schema_fields_is_system = 'is_system';
     private Module $module;
 
     public function __construct(Module $module, array $data = [])
@@ -24,12 +37,9 @@ class Taglib extends Model
         $this->module = $module;
     }
 
-    /**
-     * @inheritDoc
-     */
-    public function setup(ModelSetup $setup, Context $context): void
+    /** 标签数据同步，由 Setup/Install 在安装时调用 */
+    public function syncTaglibData(): void
     {
-        $this->install($setup, $context);
         $tagLibs = \Weline\Taglib\Helper\Taglib::getTagLibs();
         foreach ($tagLibs as $tagName => $tagLib) {
             unset($tagLib['callback']);
@@ -43,54 +53,13 @@ class Taglib extends Model
             }
             $doc = $tagLib['doc'] ?? '系统标签，请查看：' . htmlentities('<a href="https://gitee.com/aiweline/WelineFramework/wikis/%E5%BC%80%E5%8F%91%E6%96%87%E6%A1%A3/%E5%89%8D%E7%AB%AF%E5%BC%80%E5%8F%91/%E6%A0%87%E7%AD%BE/var">标签文档</a>');
             $this->clearData();
-            $this->setData(self::fields_NAME, $tagName, true)
-                ->setData(self::fields_is_system, $tagLib['is_custom'] ?? 1)
-                ->setData(self::fields_MODULE_ID, $module_id)
-                ->setData(self::fields_DESCRIPTION, $doc)
-                ->setData(self::fields_JSON, json_encode($tagLib, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT))
+            $this->setData(self::schema_fields_NAME, $tagName, true)
+                ->setData(self::schema_fields_is_system, $tagLib['is_custom'] ?? 1)
+                ->setData(self::schema_fields_MODULE_ID, $module_id)
+                ->setData(self::schema_fields_DESCRIPTION, $doc)
+                ->setData(self::schema_fields_JSON, json_encode($tagLib, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT))
                 ->save(true);
         }
     }
-
-    /**
-     * @inheritDoc
-     */
-    public function upgrade(ModelSetup $setup, Context $context): void
-    {
-        if (!$setup->tableExist()) {
-            return;
-        }
-        // 旧表可能缺少 json 列，需补加
-        if (!$setup->hasField(self::fields_JSON)) {
-            $setup->alterTable()
-                ->addColumn(
-                    self::fields_JSON,
-                    self::fields_DESCRIPTION,
-                    TableInterface::column_type_TEXT,
-                    0,
-                    '',
-                    'Taglib数据'
-                )
-                ->alter();
-        }
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function install(ModelSetup $setup, Context $context): void
-    {
-        //                $setup->dropTable();
-        if ($setup->tableExist()) {
-            return;
-        }
-        $setup->createTable('标签表')
-            ->addColumn(self::fields_ID, TableInterface::column_type_INTEGER, 11, 'primary key auto_increment', '标签ID')
-            ->addColumn(self::fields_NAME, TableInterface::column_type_VARCHAR, 60, 'unique', '标签')
-            ->addColumn(self::fields_DESCRIPTION, TableInterface::column_type_TEXT, null, '', 'Taglib 详情')
-            ->addColumn(self::fields_MODULE_ID, TableInterface::column_type_INTEGER, 11, 'not null', '标签模型ID')
-            ->addColumn(self::fields_JSON, TableInterface::column_type_TEXT, null, '', 'Taglib数据')
-            ->addColumn(self::fields_is_system, TableInterface::column_type_INTEGER, 1, 'default 0', '是否系统标签')
-            ->create();
-    }
 }
+
