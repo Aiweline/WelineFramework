ca354f41fd9fd5da6474fd48b99f2ea123a2b6fe
秋枫雁飞 <1714255949@qq.com>
2026-03-24 03:22:13 +0800

    feat(weshop): complete giftcard storefront account slice

---
+++ app/code/WeShop/GiftCard/Controller/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WeShop\GiftCard\Controller;
+
+class Index extends \WeShop\GiftCard\Controller\Frontend\GiftCard\Index
+{
+    public function index(): string
+    {
+        return parent::index();
+    }
+}
