e33736709042d5cab70cf23e9ddcccfffbf95f2c
绉嬫灚闆侀 <1714255949@qq.com>
2026-03-23 21:42:09 +0800

    feat(weshop): complete wishlist storefront slice

---
+++ app/code/WeShop/Wishlist/Controller/Remove.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WeShop\Wishlist\Controller;
+
+class Remove extends \WeShop\Wishlist\Controller\Frontend\Wishlist\Remove
+{
+    public function index(): string
+    {
+        return parent::index();
+    }
+
+    public function post(): string
+    {
+        return parent::post();
+    }
+}
