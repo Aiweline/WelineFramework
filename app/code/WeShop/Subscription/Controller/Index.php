3eac041ca9782abebfc341c53c68be1fd909c32a
绉嬫灚闆侀 <1714255949@qq.com>
2026-03-25 03:19:19 +0800

    feat(weshop): complete subscription storefront and admin flows

---
+++ app/code/WeShop/Subscription/Controller/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WeShop\Subscription\Controller;
+
+class Index extends \WeShop\Subscription\Controller\Frontend\Subscription\Index
+{
+    public function index(): string
+    {
+        return parent::index();
+    }
+}
