cfda6603a2b6e1b1b5b89ebfd323dfafd84f7e06
秋枫雁飞 <1714255949@qq.com>
2026-03-24 08:44:06 +0800

    fix(weshop): restore compare aliases and hook contracts

---
+++ app/code/WeShop/Compare/Controller/Remove.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WeShop\Compare\Controller;
+
+class Remove extends \WeShop\Compare\Controller\Frontend\Compare\Remove
+{
+    public function index(): string
+    {
+        return parent::index();
+    }
+
+    public function post(): string
+    {
+        return parent::post();
+    }
+}
