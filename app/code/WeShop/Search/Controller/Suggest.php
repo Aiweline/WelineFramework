951abd90fcfa57ac0fcf768cafa49de4d36cda37
秋枫雁飞 <1714255949@qq.com>
2026-03-24 19:58:08 +0800

    feat(weshop): complete search storefront slice

---
+++ app/code/WeShop/Search/Controller/Suggest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WeShop\Search\Controller;
+
+class Suggest extends \WeShop\Search\Controller\Frontend\Search\Suggest
+{
+    public function index(): string
+    {
+        return parent::index();
+    }
+}
