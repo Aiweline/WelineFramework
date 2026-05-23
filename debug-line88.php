3b99b5dca05513a31e2a12a30f1d62517413edf3
秋枫雁飞 <1714255949@qq.com>
2026-05-23 12:38:28 +0800

    chore: remove temporary scripts and artifacts

--- debug-line88.php
+++
@@ -1,7 +0,0 @@
-<?php
-$line = file('e:/WelineFramework/DEV-workspace/app/code/Aiweline/Community/Controller/Backend/Category.php')[87];
-$wrong = "getMessage()]\x29\x29\x29];";
-$right = "getMessage()]\x29]\x29];";
-echo 'contains wrong: ' . (str_contains($line, $wrong) ? 'yes' : 'no') . PHP_EOL;
-$pos = strpos($line, 'getMessage');
-echo 'suffix: ' . substr($line, $pos) . PHP_EOL;
