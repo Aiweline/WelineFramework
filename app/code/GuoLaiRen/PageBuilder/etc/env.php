7d1a824c12495eb52df57f6bf8bf2dbfe96a4d0c
秋枫雁飞 <1714255949@qq.com>
2026-06-08 11:27:58 +0800

    Move PageBuilder ownership to deployment repo

--- app/code/GuoLaiRen/PageBuilder/etc/env.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'router' => 'pagebuilder',
-    'backend_router' => 'pagebuilder',
-    // AI 建站流水线诊断：环境变量 PAGE_BUILDER_AI_SITE_TRACE=1，通道 pagebuilder_ai_site_trace（w_log）
-];
